a31c0260e464a616559ab7320c0f402dc18110ab
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-21 20:09:44 -0300

    Cria component cronometro

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import './src/configs/StatusBarConfig'
-import { Feather } from '@expo/vector-icons'
 
+import Cronometro from './src/pages/Cronometro'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.main}>
-        <View  style={styles.counter}>
-          <Text style={styles.counterText}>{"11:00"}</Text>
-        </View>
-      </View>
-
-      <View style={styles.footer}>
-        <TouchableOpacity style={styles.footerButton}>
-          <Feather name="play" style={styles.footerIconPlay} size={50} color="#e02041" />
-        </TouchableOpacity>
-      </View>
+      <Cronometro />
     </View>
-
   );
 }
 
@@ -27,48 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#242424',
-  },
-  main: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  counter: {
-    height: 250,
-    width: 250,
-    backgroundColor: 'transparent',
-    borderRadius: 100,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 80,
-    borderWidth: 2,
-    borderColor: '#e02041',
-  },
-  counterText: {
-    width: 200,
-    fontSize: 40,
-    color: '#e02041',
-    textAlign: 'center'
-  },
-  title: {
-    color: '#dadada',
-  },
-  footer: {
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    marginBottom: 70,
-  },
-  footerButton: {
-    width: 80,
-    height: 80,
-    borderRadius: 100,
-    backgroundColor: 'transparent',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderWidth: 2,
-    borderColor: '#e02041',
-  },
-  footerIconPlay: {
-    paddingLeft: 10
   }
 });
---
+++ mobile/src/pages/Cronometro/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons'
+
+export default function Cronometro() {
+  return (
+   <>
+      <View style={styles.main}>
+        <View  style={styles.counter}>
+          <Text style={styles.counterText}>{"11:00"}</Text>
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        <TouchableOpacity style={styles.footerButton}>
+          <Feather name="play" style={styles.footerIconPlay} size={50} color="#e02041" />
+        </TouchableOpacity>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  counter: {
+    height: 250,
+    width: 250,
+    backgroundColor: 'transparent',
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 80,
+    borderWidth: 2,
+    borderColor: '#e02041',
+  },
+  counterText: {
+    width: 200,
+    fontSize: 40,
+    color: '#e02041',
+    textAlign: 'center'
+  },
+  title: {
+    color: '#dadada',
+  },
+  footer: {
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    marginBottom: 70,
+  },
+  footerButton: {
+    width: 80,
+    height: 80,
+    borderRadius: 100,
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 2,
+    borderColor: '#e02041',
+  },
+  footerIconPlay: {
+    paddingLeft: 10
+  }
+});
