54c9c03e1e7dd6b1f109caeacdbf39822a47577d
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-21 22:16:48 -0300

    Cria nova rota para page inicial

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -5,12 +5,14 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 const AppStack = createStackNavigator()
 
+import Home from './pages/Home'
 import Cronometro from './pages/Cronometro'
 
 export default function Routes(){
     return(
         <NavigationContainer >
             <AppStack.Navigator screenOptions={{headerShown: false}}>
+                <AppStack.Screen name="Home" component={Home} />
                 <AppStack.Screen name="Cronometro" component={Cronometro} />
             </AppStack.Navigator>
         </NavigationContainer>
