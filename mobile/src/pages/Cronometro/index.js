5ad0a84d1f6f542cae6d130c993242bdce8e478c
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-21 21:20:57 -0300

    Faz cronometro funcionar

--- mobile/src/pages/Cronometro/index.js
+++ mobile/src/pages/Cronometro/index.js
@@ -1,19 +1,59 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Feather } from '@expo/vector-icons'
 
+const formatNumber = number => `0${number}`.slice(-2);
+
+  const getRemaining = (time) => {
+      const mins = Math.floor(time / 60);
+      const secs = time - mins * 60;
+      return { mins: formatNumber(mins), secs: formatNumber(secs) };
+  }
+
 export default function Cronometro() {
+  const [segundos, setSegundos] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const { mins, secs } = getRemaining(segundos);
+
+
+  
+
+  function toggle (){
+    setIsActive(!isActive);
+  }
+
+ function reset(){
+    setSegundos(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSegundos(segundos => segundos + 1);
+      }, 1000);
+    } else if (!isActive && segundos !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, segundos]);
+
+
   return (
     <View style={styles.container}>
       <View style={styles.main}>
         <View style={styles.counter}>
-          <Text style={styles.counterText}>{"11:00"}</Text>
+          <Text style={styles.counterText}>{`${mins}:${secs}`}</Text>
         </View>
       </View>
 
       <View style={styles.footer}>
-        <TouchableOpacity style={styles.footerButton}>
-          <Feather name="play" style={styles.footerIconPlay} size={50} color="#e02041" />
+        <TouchableOpacity style={styles.footerButton} onPress={toggle}>
+          <Feather name={isActive ? 'pause' : 'play'}  size={50} color="#e02041" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerButton} onPress={reset}>
+          <Feather name="x"  size={50} color="#e02041" />
         </TouchableOpacity>
       </View>
     </View>
@@ -37,7 +77,6 @@ const styles = StyleSheet.create({
     borderRadius: 100,
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: 80,
     borderWidth: 2,
     borderColor: '#e02041',
   },
@@ -51,11 +90,13 @@ const styles = StyleSheet.create({
     color: '#dadada',
   },
   footer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-end',
+    justifyContent: 'center',
     marginBottom: 70,
   },
   footerButton: {
+    margin: 15,
     width: 80,
     height: 80,
     borderRadius: 100,
@@ -65,7 +106,5 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: '#e02041',
   },
-  footerIconPlay: {
-    paddingLeft: 10
-  }
+ 
 });
